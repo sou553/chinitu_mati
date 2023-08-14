e2baaef49951ba5f0c4953dae0228c705118db26
sou553 <80958557+sou553@users.noreply.github.com>
2023-08-14 22:56:05 +0900

    Update chinitu_main.py

--- chinitu_main.py
+++ chinitu_main.py
@@ -6,29 +6,28 @@ import random
 
 
 def true(tehai):
-    pygame.init()#初期化
+    pygame.init()
     (w, h) = 900, 600
     (x, y) = (w/2, h/2)
     pygame.display.set_mode((w, h), 0, 32)
     screen = pygame.display.get_surface()
-    pygame.display.set_caption("True Page") # ウィンドウの上の方に出てくるアレの指定
+    pygame.display.set_caption("True Page")
 
     mati = check.matihai(tehai)
     x = 450
     y = 300
-    title = pygame.image.load("gif/maru.png").convert_alpha() # 画像の指定
-    rect_title = title.get_rect() # 謎
+    title = pygame.image.load("gif/maru.png").convert_alpha()
+    rect_title = title.get_rect()
     rect_title.center = (x, y)
 
     while(True):
-        screen.fill((0, 0, 0, 0)) # 背景色の指定。RGBのはず
-        #screen.blit(bg, rect_bg) # 背景画像の描画
+        screen.fill((0, 0, 0, 0))
         screen.blit(title, rect_title)
         
-        pygame.time.wait(10) # 更新間隔。多分ミリ秒
-        pygame.display.update() # 画面更新
+        pygame.time.wait(10)
+        pygame.display.update()
 
-        for event in pygame.event.get(): # 終了処理
+        for event in pygame.event.get(): 
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     sys.exit()
@@ -37,29 +36,28 @@ def true(tehai):
 
 
 def false(tehai):
-    pygame.init()#初期化
+    pygame.init()
     (w, h) = 900, 600
     (x, y) = (w/2, h/2)
     pygame.display.set_mode((w, h), 0, 32)
     screen = pygame.display.get_surface()
-    pygame.display.set_caption("False Page") # ウィンドウの上の方に出てくるアレの指定
+    pygame.display.set_caption("False Page")
 
     mati = check.matihai(tehai)
     x = 450
     y = 300
-    title = pygame.image.load("gif/batu.png").convert_alpha() # 画像の指定
-    rect_title = title.get_rect() # 謎
+    title = pygame.image.load("gif/batu.png").convert_alpha() 
+    rect_title = title.get_rect() 
     rect_title.center = (x, y)
 
     while(True):
-        screen.fill((0, 0, 0, 0)) # 背景色の指定。RGBのはず
-        #screen.blit(bg, rect_bg) # 背景画像の描画
+        screen.fill((0, 0, 0, 0))
         screen.blit(title, rect_title)
         
-        pygame.time.wait(10) # 更新間隔。多分ミリ秒
-        pygame.display.update() # 画面更新
+        pygame.time.wait(10) 
+        pygame.display.update() 
 
-        for event in pygame.event.get(): # 終了処理
+        for event in pygame.event.get():
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     sys.exit()
@@ -68,16 +66,16 @@ def false(tehai):
 
 
 def answer(tehai):
-    pygame.init()#初期化
+    pygame.init()
     (w, h) = 900, 600
     (x, y) = (w/2, h/2)
     pygame.display.set_mode((w, h), 0, 32)
     screen = pygame.display.get_surface()
-    pygame.display.set_caption("True Page") # ウィンドウの上の方に出てくるアレの指定
+    pygame.display.set_caption("Answer Page")
 
     mati = check.matihai(tehai)
-    title = pygame.image.load("gif/kotae.png").convert_alpha() # 画像の指定
-    rect_title = title.get_rect() # 謎
+    title = pygame.image.load("gif/kotae.png").convert_alpha()
+    rect_title = title.get_rect()
     rect_title.center = (230, 100)
     n = 0
     x = 150
@@ -101,21 +99,20 @@ def answer(tehai):
         n += 1
 
     while(True):
-        screen.fill((0, 0, 0, 0)) # 背景色の指定。RGBのはず
-        #screen.blit(bg, rect_bg) # 背景画像の描画
+        screen.fill((0, 0, 0, 0))
         screen.blit(title, rect_title)
         
         for i in range(len(mati)):
-            exec("screen.blit(hai_" + str(i) + ", rect_hai_" + str(i) + ")") # 描画
+            exec("screen.blit(hai_" + str(i) + ", rect_hai_" + str(i) + ")")
         for i in range(len(tehai)):
-            exec("screen.blit(hai_m_" + str(i) + ", rect_hai_m_" + str(i) + ")") # 描画
+            exec("screen.blit(hai_m_" + str(i) + ", rect_hai_m_" + str(i) + ")")
         
         
-        pygame.time.wait(10) # 更新間隔。多分ミリ秒
-        pygame.display.update() # 画面更新
+        pygame.time.wait(10)
+        pygame.display.update()
 
 
-        for event in pygame.event.get(): # 終了処理
+        for event in pygame.event.get():
             if event.type == MOUSEBUTTONDOWN:
                 main_loop()
             if event.type == KEYDOWN:
@@ -124,19 +121,17 @@ def answer(tehai):
 
 
 def main(tehai):
-    pygame.init() # 初期化
+    pygame.init()
     (w, h) = (900,600)
     (x, y) = (w/2, h/2)
     pygame.display.set_mode((w, h), 0, 32)
     screen = pygame.display.get_surface()
-    pygame.display.set_caption("Pygame Test") # ウィンドウの上の方に出てくるアレの指定
-    #bg = pygame.image.load("gif/1.png").convert_alpha() # 背景画像の指定
-    #rect_bg = bg.get_rect() # 画像のサイズ取得？？だと思われる
-    ans_button = pygame.image.load("gif/ans.png").convert_alpha() # キャラ画像の指定
-    rect_ans_button = ans_button.get_rect() # 謎
+    pygame.display.set_caption("Pygame Test")
+    ans_button = pygame.image.load("gif/ans.png").convert_alpha()
+    rect_ans_button = ans_button.get_rect()
     rect_ans_button.center = (400, 350)
-    title = pygame.image.load("gif/title.png").convert_alpha() # キャラ画像の指定
-    rect_title = title.get_rect() # 謎
+    title = pygame.image.load("gif/title.png").convert_alpha()
+    rect_title = title.get_rect()
     rect_title.center = (230, 50)
     
     list_type = [0,0,0,0,0,0,0,0,0]
@@ -167,20 +162,19 @@ def main(tehai):
             x += 50
             n += 1
 
-        screen.fill((0, 0, 0, 0)) # 背景色の指定。RGBのはず
-        #screen.blit(bg, rect_bg) # 背景画像の描画
+        screen.fill((0, 0, 0, 0))
         screen.blit(ans_button, rect_ans_button)
         screen.blit(title, rect_title)
         l = len(tehai)
         for i in range(l):
-            exec("screen.blit(hai_" + str(i) + ", rect_hai_" + str(i) + ")") # キャラの描画
+            exec("screen.blit(hai_" + str(i) + ", rect_hai_" + str(i) + ")")
         for i in range(1,10):
-            exec("screen.blit(hai_m_" + str(i) + ", rect_hai_m_" + str(i) + ")") # キャラの描画
-        pygame.time.wait(10) # 更新間隔。多分ミリ秒
-        pygame.display.update() # 画面更新
+            exec("screen.blit(hai_m_" + str(i) + ", rect_hai_m_" + str(i) + ")")
+        pygame.time.wait(10)
+        pygame.display.update()
 
 
-        for event in pygame.event.get(): # 終了処理
+        for event in pygame.event.get():
             if event.type == MOUSEBUTTONDOWN:
                 x, y = event.pos
                 #print(x,y)
